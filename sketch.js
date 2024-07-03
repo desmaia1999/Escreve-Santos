4c757e81202d2e8bb0b31807980ff5d0ab6061fb
desmaia1999 <luh.carol.maia@gmail.com>
2024-07-03 12:06:22 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
+let palavra;
 function setup() {
   createCanvas(400, 400);
-  background(220);
+  palavra = palavraAleatoria ();
 }
-
+function palavraAleatoria () {
+ let palavras = ["Santos","Santástico","Sensacional"];
+  return random(palavras); 
+}
+function inicializaCores () {
+background("black");
+fill ("white");
+textSize (64);
+textAlign (CENTER,CENTER) 
+}
+function palavraParcial (minimo,maximo) {
+ let quantidade = map (mouseX, minimo, maximo, 1, palavra.length);
+  let parcial = palavra.substring (0, quantidade);
+  return parcial;
+}
 function draw() {
-  fill("rgb(0,50,255)");
-  stroke("black");
-  strokeWeight(5);
-  
-  if(mouseIsPressed){
-    rect(mouseX,mouseY,50,70);
-  }
+inicializaCores ()
+  let texto = palavraParcial (0,width);
+  text (texto, 200,200);
 }
